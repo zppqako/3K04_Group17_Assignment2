34be32edc696254c17c20bfe24a7fd2fd5457815
Allen-ENGI <tcl23@163.com>
2023-11-28 22:17:14 -0500

    recieve

--- DCM_serial.py
+++ DCM_serial.py
@@ -19,14 +19,8 @@ SYNC = b'\x22'
 
 Fn_set = b'\x55'
 
-# ser = serial.Serial()
-ser = serial.Serial()
-# ser.baudrate = 115200
-# ser = serial.Serial('COM12', baudrate=115200)
-
-
-# def input():
-def input(lrl, url, amplitude, pw, rp, factor_input, threshold, mode):
+def input():
+# def input(lrl, url, amplitude, pw, rp, factor_input, threshold, mode):
     Start = b'\x16'
     print("here")
     # second bit
@@ -46,6 +40,16 @@ def input(lrl, url, amplitude, pw, rp, factor_input, threshold, mode):
     # max_input = 150
     # factor_input = 30
     
+    mode = 5
+    lrl = 60
+    url = 150
+    amplitude = 80
+    pw = 2
+    rp = 150
+    factor_input = 10
+    threshold = 80
+    
+    
     mode = struct.pack("B", int(mode))
     # rate = struct.pack("BB", 0, 1)
     rate = int(lrl*2).to_bytes(2, byteorder='little')
@@ -58,43 +62,63 @@ def input(lrl, url, amplitude, pw, rp, factor_input, threshold, mode):
     
     
     signal_echo = Start + Fn_set + mode + rate + pw + apl + rp+ thre + max + factor
+    # signal_echo = Start + SYNC + mode + rate + pw + apl + rp+ thre + max + factor
+    
     print("test: ", signal_echo)
 
     with serial.Serial(frdm_port, 115200) as pacemaker:
         print("Connect")
         pacemaker.write(signal_echo)
 
+    
 # input()
+
 
 # def receive():
 #     Start = b'\x16'
 #     SYNC = b'\x22'
 #     Fn_set = b'\x55'
-#     # Signal_echo = b'\x16\x22'# + b'\00' * 17
+#     mode = struct.pack("B", int(0))
 #     Signal_echo = Start + SYNC
-#     # i=0
-#     # while(i<72):
-#     #     Signal_echo = Signal_echo + struct.pack("B", 0)
-#     #     i = i+1
 
-#     with serial.Serial(frdm_port, 115200) as pacemaker:
-#         try:
-#             print("reading")
-#             pacemaker.write(Signal_echo)
-#             time.sleep(0.1)
-#             print("keep reading")
-            
-#             data = pacemaker.read(16)
-#             print("afer reading")
-#             ATR_signal = struct.unpack("d", data[0:8])[0]
-#             VENT_signal = struct.unpack("d", data[8:16])[0]
-            
-#             print("finish reading")
-#             return [ATR_signal,VENT_signal]
-#         except Exception as e:
-#             print("wrong")
+
+#     i=0
+#     while(i<15):
+#         Signal_echo = Signal_echo + struct.pack("B", 0)
+#         i = i+1
+
+#     with serial.Serial(frdm_port, 115200, timeout=1) as pacemaker:
+#         pacemaker.reset_input_buffer()
+#         pacemaker.reset_output_buffer()
+        
+        
+#         # print("reading")
+#         pacemaker.write(Signal_echo)
+#         # time.sleep(1)
+
+#         data = pacemaker.read(16)
+#         # print(data[0])
+#         # print("afer reading")
+#         # mode = struct.unpack("b", data[0])
+#         # print(mode)
+#         # rate = struct.unpack("B", data[1:2])
+#         # print(rate)
+#         ATR_signal = struct.unpack("d", data[0:8])[0]
+#         # print(ATR_signal)
+#         VENT_signal = struct.unpack("d", data[8:16])[0]
+
+#         # sig1 = ATR_signal
+#         # sig2 = VENT_signal
+        
+#         print("finish reading")
+#         # return [sig1,sig2]
+#         print([ATR_signal,VENT_signal])
     
+# 
+# input()
 # receive()
-
+# i = 0
+# while i < 100:
+#     receive()
+#     i+=1
 # print(result)
-
